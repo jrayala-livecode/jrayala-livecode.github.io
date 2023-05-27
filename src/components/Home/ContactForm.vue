056e72c44a47ec3ee97db70d44408d62a95c6d4e
Joaquín <joaquin.ayala.c@gmail.com>
2023-05-27 03:29:07 -0400

    Agregar linkedIn y Github a Portafolio.

--- src/components/Home/ContactForm.vue
+++ src/components/Home/ContactForm.vue
@@ -47,10 +47,10 @@
                                 <!-- <li><a href="{{ twitter }}" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li> -->
                                 <!-- <li><a href="{{  }}" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a>
                                 </li> -->
-                                <li><a href="#" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+                                <li><a href="https://github.com/jrayala-livecode" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
                                 <!-- <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a>
                                 </li> -->
-                                <li><a href="" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a>
+                                <li><a href="https://www.linkedin.com/in/jrayalac/" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a>
                                 </li>
                             </ul>
                         </li>
